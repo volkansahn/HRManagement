1d9effd3e88d9b12379f8e347c736c195508d4d7
Utku Burgaz <utkuburgaz@gmail.com>
2021-07-20 13:26:30 +0300

    Dismiss Keyboard
    - Fixed to dismiss keyboard on login screen

--- IKYonetim/Controller/LoginViewController.swift
+++ IKYonetim/Controller/LoginViewController.swift
@@ -19,8 +19,20 @@ class LoginViewController: UIViewController {
     let keychain = KeychainSwift()
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Looks for single or multiple taps.
+             let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
 
+            //Uncomment the line below if you want the tap not not interfere and cancel other interactions.
+            //tap.cancelsTouchesInView = false
+
+            view.addGestureRecognizer(tap)
         // Do any additional setup after loading the view.
+    }
+    
+    //Calls this function when the tap is recognized.
+    @objc func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
     }
     
     @IBAction func loginPressed(_ sender: UIButton) {
